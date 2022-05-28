7888b322d4f307401dadf222d29ed2a22420fa95
Chinni-Chinni <enjarappachinni2001@gmil.com>
2022-05-28 17:00:50 +0530

    commited

---
+++ uniteval/index.js
@@ -0,0 +1,13 @@
+// const getData = async() =>{
+//     try {
+//         const res = await fetch(`http://localhost:3000/todo`);
+//         const data = await res.json();
+//         console.log(data);
+        
+//     } catch (error) {
+//         console.log(err);
+//     }
+    
+// }
+// getData();
+
